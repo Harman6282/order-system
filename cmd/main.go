fe0c86b19add31b77f0811608b35e6c11657692e
Harmans <harmans6282@gmail.com>
2026-02-20 11:23:20 +0530

    basic server init

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,40 @@
-package main 
+package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
 
 func main() {
-	
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	cfg := config{
+		addr: ":8080",
+	}
+
+	app := &application{
+		config: cfg,
+	}
+
+	r.Get("/", app.health)
+
+	fmt.Println("server started at :8080")
+	err := http.ListenAndServe(app.config.addr, r)
+	if err != nil {
+		log.Fatalf("error starting server: %v", err)
+	}
+
 }
